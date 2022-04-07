b8fba1f4f79c26676b660b661f650d60311e86ab
Augustas Lingys <augustas.lingys@ingeniousit.lt>
2022-04-07 23:15:30 +0300

    hide user password for response

--- src/main/java/task/weatherforecast/user/entity/User.java
+++ src/main/java/task/weatherforecast/user/entity/User.java
@@ -1,12 +1,12 @@
 package task.weatherforecast.user.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -24,11 +24,11 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private String firstName;
     private String lastName;
     private String homeLocation;
-    //private List<City> favoriteCities;
 
     private Boolean locked;
     private Boolean enabled;
